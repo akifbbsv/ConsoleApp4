1e9357cd83557f7a12250387d7921abf04bd28da
Asus <Asus@DESKTOP-JETP3H0>
2023-01-14 09:47:34 +0400

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,32 @@
+﻿using Metodlar;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Urun urun1 = new Urun();
+            urun1.Adi = "Qarpiz";
+            urun1.Fiyati = 10;
+            urun1.Aciklama = "Sabirabad Qarpizi";
+            Urun urun2 = new Urun();
+            urun2.Adi = "Alma";
+            urun2.Fiyati = 5;
+            urun2.Aciklama = "Quba Almasi";
+            Urun[] urunler = new Urun[] {urun1,urun2 };
+            foreach (var urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("------------");
+            }
+            Console.WriteLine("----Metodlar-----");
+            Sepetmanagar sepetmanagar = new Sepetmanagar();
+            sepetmanagar.Ekle(urun1);
+            sepetmanagar.Ekle(urun2);
+           
+        }
+    }
+}
